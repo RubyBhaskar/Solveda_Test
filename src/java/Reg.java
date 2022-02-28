1422dda019f6d0691262191d72cfee11b64600a4
RubyBhaskar <100565369+RubyBhaskar@users.noreply.github.com>
2022-02-28 16:06:50 +0530

    Initial commit

---
+++ src/java/Reg.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author rsingh
+ */
+public class Reg extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String n=request.getParameter("userName");  
+String p=request.getParameter("userPass");  
+try{  
+Class.forName("oracle.jdbc.driver.OracleDriver");  
+Connection con=DriverManager.getConnection(  
+"jdbc:oracle:thin:@localhost:1521:xe","system","123");  
+  
+PreparedStatement ps=con.prepareStatement(  
+"insert into emp values(?,?)");  
+  
+ps.setString(1,n);  
+ps.setString(2,p);  
+
+          
+int i=ps.executeUpdate();  
+if(i>0)  
+out.print("You are successfully registered...");  
+      
+          
+}catch (Exception e2) {System.out.println(e2);}  
+          
+out.close();  
+           
+        }
+    }}
